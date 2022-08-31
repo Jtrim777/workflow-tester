0aca2699c657a25343e6633bb577fef46d4000fe
Jake Trimble <jatrimble777@gmail.com>
2022-08-31 15:36:05 -0700

    Ensure send JSON

--- .github/scripts/update_pr.py
+++ .github/scripts/update_pr.py
@@ -13,7 +13,7 @@ repo_path = sys.argv[2]
 pr_num = sys.argv[3]
 url = "https://api.github.com/repos/%s/pulls/%s" % (repo_path, pr_num)
 
-result = requests.patch(url, data=data, headers=headers)
+result = requests.patch(url, json=data, headers=headers)
 
 if result.status_code > 299 or result.status_code < 200:
     print("\u001b[31mError %i: Failed to modify PR title: %s\u001b[0m" % (result.status_code, result.text))
